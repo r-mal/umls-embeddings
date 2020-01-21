1ac29b15f5bedf8edbd2c90d0ccdae094be03518
r-mal <ramon@hlt.utdallas.edu>
2020-01-21 11:14:53 -0600

    fixed embeddings file

--- python/eukg/Config.py
+++ python/eukg/Config.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 
 # control options
-tf.flags.DEFINE_string("data_dir", "data", "training data directory")
-tf.flags.DEFINE_string("model_dir", "model", "model directory")
-tf.flags.DEFINE_string("model", "transe", "Model: [transe, transd, distmult]")
+tf.flags.DEFINE_string("data_dir", "/users/rmm120030/working/umls-mke/new", "training data directory")
+tf.flags.DEFINE_string("model_dir", "/users/rmm120030/working/umls-mke/new/model", "model directory")
+tf.flags.DEFINE_string("model", "transd", "Model: [transe, transd, distmult]")
 tf.flags.DEFINE_string("mode", "disc", "Mode: [disc, gen, gan]")
 tf.flags.DEFINE_string("run_name", None, "Run name")
-tf.flags.DEFINE_string("summaries_dir", "data/summary", "model summary dir")
-tf.flags.DEFINE_integer("batch_size", 1024, "batch size")
+tf.flags.DEFINE_string("summaries_dir", "/users/rmm120030/working/umls-mke/new/model", "model summary dir")
+tf.flags.DEFINE_integer("batch_size", 564, "batch size")
 tf.flags.DEFINE_bool("load", False, "Load model?")
 tf.flags.DEFINE_bool("load_embeddings", False, "Load embeddings?")
-tf.flags.DEFINE_string("embedding_file", "data/embeddings.npz", "Embedding matrix npz")
+tf.flags.DEFINE_string("embedding_file", None, "Embedding matrix npz")
 tf.flags.DEFINE_integer("num_epochs", 100, "Number of epochs?")
 tf.flags.DEFINE_float("val_proportion", 0.1, "Proportion of training data to hold out for validation")
 tf.flags.DEFINE_integer("progress_update_interval", 1000, "Number of batches between progress updates")
-tf.flags.DEFINE_integer("val_progress_update_interval", 680, "Number of batches between progress updates")
+tf.flags.DEFINE_integer("val_progress_update_interval", 1800, "Number of batches between progress updates")
 tf.flags.DEFINE_integer("save_interval", 1800, "Number of seconds between model saves")
 tf.flags.DEFINE_integer("batches_per_epoch", 1, "Number of batches per training epoch")
 tf.flags.DEFINE_integer("max_batches_per_epoch", None, "Maximum number of batches per training epoch")
@@ -40,7 +40,7 @@ tf.flags.DEFINE_float("learning_rate", 1e-5, "Starting learning rate")
 tf.flags.DEFINE_float("decay_rate", 0.96, "LR decay rate")
 tf.flags.DEFINE_float("momentum", 0.9, "Momentum")
 tf.flags.DEFINE_float("gamma", 0.1, "Margin parameter for loss")
-tf.flags.DEFINE_integer("vocab_size", 1726933, "Number of unique concepts+relations")
+tf.flags.DEFINE_integer("vocab_size", 3210965, "Number of unique concepts+relations")
 tf.flags.DEFINE_integer("embedding_size", 50, "embedding size")
 tf.flags.DEFINE_integer("energy_norm_ord", 1,
                         "Order of the normalization function used to quantify difference between h+r and t")
@@ -56,7 +56,7 @@ tf.flags.DEFINE_float("semnet_energy_param", 0.5, "Parameter to control semantic
 tf.flags.DEFINE_bool("sn_eval", False, "Train this model with subset of sn to evaluate the SN embeddings?")
 
 # distmult params
-tf.flags.DEFINE_float("regularization_parameter", 1e-2, "Regularization term weight")
+tf.flags.DEFINE_float("regularization_parameter", 1e-4, "Regularization term weight")
 tf.flags.DEFINE_string("energy_activation", 'sigmoid',
                        "Energy activation function [None, tanh, relu, sigmoid]")
 
